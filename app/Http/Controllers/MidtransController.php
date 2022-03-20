af5c571da52a1e00216b78f88fa036f094bf298d
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-03-20 17:46:22 +0700

    bug fixing controller

--- app/Http/Controllers/MidtransController.php
+++ app/Http/Controllers/MidtransController.php
@@ -6,58 +6,61 @@ use Illuminate\Http\Request;
 
 class MidtransController extends Controller
 {
-    //call initialize midtrans payment gateway
-    $this->initMidtrans();
+    public function paymentgateway()
+    {
+        //call initialize midtrans payment gateway
+        $this->initMidtrans();
 
-    $params = array(
-        'transaction_details' => array(
-            'order_id' => rand(),
-            'gross_amount' => 10000,
-        ),
-
-        'item_details' => array(
-            array(
-                'id' => 'ITEM1',
-                'price' => 10000,
-                'quantity' => 1,
-                'name' => 'abc',
-                'brand' => 'def',
-                'categoty' => 'ghi',
-                'merchant_name' => 'jkl',
-            )
-        ),
-
-        'customer_details' => array(
-            'first_name' => 'dobleh',
-            'last_name' => 'jamal',
-            'email' => 'abc@abc.com',
-            'phone' => '+6213456',
-
-            'billing_address' => array(
-                'first_name' => 'dobleh',
-                'last_name' => 'jamal',
-                'email' => 'abc@abc.com',
-                'phone' => '+6213456',
-                'address' => 'aaa',
-                'city' => 'bbb',
-                'postal_code' => '123',
-                'country_code' => 'IDN'
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => rand(),
+                'gross_amount' => 10000,
             ),
 
-            'shipping_address' => array(
+            'item_details' => array(
+                array(
+                    'id' => 'ITEM1',
+                    'price' => 10000,
+                    'quantity' => 1,
+                    'name' => 'abc',
+                    'brand' => 'def',
+                    'categoty' => 'ghi',
+                    'merchant_name' => 'jkl',
+                )
+            ),
+
+            'customer_details' => array(
                 'first_name' => 'dobleh',
                 'last_name' => 'jamal',
                 'email' => 'abc@abc.com',
                 'phone' => '+6213456',
-                'address' => 'aaa',
-                'city' => 'bbb',
-                'postal_code' => '123',
-                'country_code' => 'IDN'
+
+                'billing_address' => array(
+                    'first_name' => 'dobleh',
+                    'last_name' => 'jamal',
+                    'email' => 'abc@abc.com',
+                    'phone' => '+6213456',
+                    'address' => 'aaa',
+                    'city' => 'bbb',
+                    'postal_code' => '123',
+                    'country_code' => 'IDN'
+                ),
+
+                'shipping_address' => array(
+                    'first_name' => 'dobleh',
+                    'last_name' => 'jamal',
+                    'email' => 'abc@abc.com',
+                    'phone' => '+6213456',
+                    'address' => 'aaa',
+                    'city' => 'bbb',
+                    'postal_code' => '123',
+                    'country_code' => 'IDN'
+                )
+                
             )
-            
-        )
-    );
-    
-    $snapToken = \Midtrans\Snap::getSnapToken($params);
-    return view('bayar', ['snapToken' => $snapToken]);
+        );
+        
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        return view('home', ['snapToken' => $snapToken]);
+    }
 }
