c1322bc4c7a581585b6d83aa009b4359d160c881
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-03-20 18:14:13 +0700

    tes route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home',[MidtransController::class,'paymentgateway'])->name('payment');
+Route::get('/home',[MidtransController::class,'paymentgateway']);
